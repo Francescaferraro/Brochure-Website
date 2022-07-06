af716e39877dccfd6d7c5c9e536a65eaa5980ea4
Francesca Ferraro <francescaferraro1707@gmail.com>
2022-07-06 17:17:50 +0200

    Add files via upload

---
+++ edilklima.js
@@ -0,0 +1,28 @@
+function showInfo() {
+	document.getElementById('show-numero').style.display = "none";
+	document.getElementById('show-orario').style.display = "none";
+	document.getElementById('show-email').style.display = "none";
+   document.getElementById('show-indirizzo').style.display = "block";
+}
+
+function showNum() {
+	document.getElementById('show-indirizzo').style.display = "none";
+	document.getElementById('show-orario').style.display = "none";
+	document.getElementById('show-email').style.display = "none";
+   document.getElementById('show-numero').style.display = "block";
+}
+
+function showTime() {
+	document.getElementById('show-numero').style.display = "none";
+	document.getElementById('show-indirizzo').style.display = "none";
+	document.getElementById('show-email').style.display = "none";
+   document.getElementById('show-orario').style.display = "block";
+}
+
+function showEmail() {
+	document.getElementById('show-numero').style.display = "none";
+	document.getElementById('show-orario').style.display = "none";
+	document.getElementById('show-indirizzo').style.display = "none";
+   document.getElementById('show-email').style.display = "block";
+}
+
